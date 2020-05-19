24509aba526e95f42a683805aa79a89cd047fef8
Tinashe Dan <tinashydaniel@gmail.com>
2020-05-20 01:49:25 +0200

    sticky navbar offset rule tweak

--- public/resources/js/script.js
+++ public/resources/js/script.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
 
+  /* For the sticky navigation */
   $('.js--section-features').waypoint(function (direction) {
     if(direction == "down") {
       $('nav').addClass('sticky');
     } else {
       $('nav').removeClass('sticky');
     }
+  }, {
+    //this triggers the event 60px before we get to the html element we targeted with the .js--section-features class;
+    offset: '60px;'
   });
 
-}, {
-  //this triggers the event 60px before we get to the html element we targeted with the .js--section-features class;
-  offset: '60px;'
 });
