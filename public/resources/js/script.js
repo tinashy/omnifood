cd04f1f2c35b2c5087a78a9a666eff4adb3a7639
Tinashe Dan <tinashydaniel@gmail.com>
2020-05-20 01:39:06 +0200

    sticky navigation part 2

--- public/resources/js/script.js
+++ public/resources/js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
-  $('h1').click(function () {
-    $(this).css('background-color', '#ff0000');
+
+  $('.js--section-features').waypoint(function (direction) {
+    if(direction == "down") {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
   });
+
 });
